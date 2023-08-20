af045b77e9de222a8345bc32d5e1473e50a00ac2
ryanfadhilah <ryan.fadhilah.acc@gmail.com>
2023-08-20 23:51:14 +0700

    search done

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { fetchUser } from "@/lib/actions/user/userFetch.actions";
+import {
+  fetchUser,
+  fetchUserTweetActivity,
+} from "@/lib/actions/user/userFetch.actions";
 
 async function Page() {
   const user = await currentUser();
@@ -11,39 +14,39 @@ async function Page() {
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
-  //   const activity = await getActivity(userInfo._id);
+  const activity = await fetchUserTweetActivity(userInfo._id);
 
   return (
     <>
       <h1 className="head-text">Activity</h1>
 
-      {/* <section className='mt-10 flex flex-col gap-5'>
+      <section className="mt-10 flex flex-col gap-5">
         {activity.length > 0 ? (
           <>
             {activity.map((activity) => (
-              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
-                <article className='activity-card'>
+              <Link key={activity._id} href={`/tweet/${activity.parentId}`}>
+                <article className="activity-card">
                   <Image
                     src={activity.author.image}
-                    alt='user_logo'
+                    alt="user_logo"
                     width={20}
                     height={20}
-                    className='rounded-full object-cover'
+                    className="rounded-full object-cover"
                   />
-                  <p className='!text-small-regular text-light-1'>
-                    <span className='mr-1 text-primary-500'>
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
                       {activity.author.name}
                     </span>{" "}
-                    replied to your thread
+                    replied to your tweet
                   </p>
                 </article>
               </Link>
             ))}
           </>
         ) : (
-          <p className='!text-base-regular text-light-3'>No activity yet</p>
+          <p className="!text-base-regular text-light-3">No activity yet</p>
         )}
-      </section> */}
+      </section>
     </>
   );
 }
